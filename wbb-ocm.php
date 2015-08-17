99c95d19214304c0676bdc718eeca0dfec109a73
Francisco Palacios Chaves <fpalacioschaves@gmail.com>
2015-08-17 16:07:29 +0200

    Change text domain

--- wbb-ocm.php
+++ wbb-ocm.php
@@ -21,7 +21,7 @@
  * Author URI:        http://webberty.com
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       wbb-ocm
+ * Text Domain:       wbb-offcanvas-menu
  * Domain Path:       /languages
  */
 
